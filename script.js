3260b33922c4112d9626871fd92d1ab247b012cb
unknown <charles.pitek@gmail.com>
2019-12-26 14:28:23 -0800

    added functionallity to the buttons. still need to write the save function

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-// function plannerPop() {
-//     var planner = $(".planner")
-//     for (var i = 9; i <= 17; i++) {
-//         if (i > 12) {
-//             var fill = $("<div class='time'>" + (i - 12) + " pm</div><div class='plan></div><div class='saveButton'></div>")
-//             planner.append(fill);
-//         } else if (i === 12) {
-//             var fill = $("<div class='time'>12 pm</div><div class='plan></div><div class='saveButton'></div>")
-//             planner.append(fill);
-//         } else {
-//             var fill = $("<div class='time'>" + i + " am</div><div class='plan></div><div class='saveButton'></div>")
-//             planner.append(fill);
-
-//         }
-//     }
-// }
-
-// plannerPop();
-
 function saveClick () { //function that is run when save button is clicked
-
+    console.log("Mic check. is this working.")
 }
 
 function pullStorage () { //function to get info from local storage
 
 }
+
+$(".planner").on("click", "button", saveClick)
