399a175ba998d5dd3f8473d53f9ddcd62cd2aa0e
unknown <charles.pitek@gmail.com>
2019-12-26 15:10:37 -0800

    got function to pull needed info out of the input field

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function saveClick () { //function that is run when save button is clicked
-    console.log("Mic check. is this working.")
+    console.log(this);
+    var test = $(this).attr("data-time")
+    var test2 = $(this).siblings();
+    var test3 = test2[1].value;
+    console.log(test);
+    console.log(test2)
+    // var test3 = $(test2).val();
+    console.log(test3);
 }
 
 function pullStorage () { //function to get info from local storage
