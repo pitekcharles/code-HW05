c0f8a284158e2cd40ee9d825492cbb72e5e65d7a
unknown <charles.pitek@gmail.com>
2019-12-26 15:22:05 -0800

    added some comments to make returning to this code easier

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ function saveClick () { //function that is run when save button is clicked
     console.log(test3);
     saveAry[test] = test3;
     console.log(saveAry);
+
+    //need to rename some variables and convert array into string and store in local storage.
 }
 
-function pullStorage () { //function to get info from local storage
+function pullStorage () { //function to get info from local storage and populate input fields to contain info from storage
 
 }
 
