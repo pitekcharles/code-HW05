208cd7a214ab0beb06de09dabdebc424e2f10493
unknown <charles.pitek@gmail.com>
2019-12-26 15:17:23 -0800

    added in array and made function push save data to array

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+var saveAry = [];
+
 function saveClick () { //function that is run when save button is clicked
     console.log(this);
     var test = $(this).attr("data-time")
     var test2 = $(this).siblings();
     var test3 = test2[1].value;
     console.log(test);
-    console.log(test2)
-    // var test3 = $(test2).val();
+    console.log(test2);
     console.log(test3);
+    saveAry[test] = test3;
+    console.log(saveAry);
 }
 
 function pullStorage () { //function to get info from local storage
