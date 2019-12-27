fd2b7cbb91afd117bc49aadae192682461bd65a4
unknown <charles.pitek@gmail.com>
2019-12-27 10:26:07 -0800

    added code to save values to local storage and change colors of sections based on what hour it is

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var saveAry = [];
+var printAry = [];
+
 
 //---------------------------------------------------
 // these lines create variables using moment.js
@@ -8,7 +10,7 @@ var saveAry = [];
 // formatting.
 //---------------------------------------------------
 var dayDate = moment().format("dddd, MMMM Do YYYY");
-var currentHour = moment().format("hh A");
+var currentHour = moment().format("HH");
 
 console.log(currentHour);
 console.log(dayDate);
@@ -26,12 +28,31 @@ function saveClick () { //function that is run when save button is clicked
     console.log(test3);
     saveAry[time] = test3;
     console.log(saveAry);
+    var storageAry = saveAry.toString();
+    localStorage.setItem("Planner Save", storageAry);
+    pullStorage();
 
     //need to rename some variables and convert array into string and store in local storage.
 }
 
 function pullStorage () { //function to get info from local storage and populate input fields to contain info from storage
+    console.log("pullStorage test");
+    printAry = localStorage.getItem("Planner Save");
+    printAry = printAry.split(",");
+    console.log(printAry);
+    for (var i = 9; i<printAry.length; i++){
+        var temp =document.getElementById(i);
+        console.log(temp);
+        $(temp).val(printAry[i]);
+        if (i == currentHour) {
+            $(temp).css("background", "red")
+        } else if (i < currentHour) {
+            $(temp).css("background", "grey")
+        }
+    }
 
+    
 }
-
+pullStorage();
+saveAry = printAry;
 $(".planner").on("click", "button", saveClick)
