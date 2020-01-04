48c916bc23744d2a227b807c7fd0f88340e90f73
unknown <charles.pitek@gmail.com>
2020-01-04 14:33:27 -0800

    removed unneeded console.logs and added info to the read me

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# code-HW05
+# code-HW05 Day Planner - Charles Pitek
+
+## About
+This is a in browser day planner.
+
+## Use
+To use the day planner open it up in your browser.
+
+Once in the planner you will see the header and then below that is a line for each hour of a normal work day.
+the left side shows the time, the middle is where you can enter tasks and the right side is a button to save tasks to the planner.
+
+To save a task simply type the task into the text area and click the save button. once tasks have been saved the hours will color code to grey if passes, red for current, and green for upcoming. When saving tasks do not use any commas (","). This will cause everything after the comma to populate an hour later.
+
+The planner will clear on a new day.
+
+## Technology
+This project uses moment.js for time related things. Also uses HTML, css, javascript, and jQuery.
+
+### This is a Charles Pitek Project.
--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 var saveAry = ["","","","","","","","","","","","","","","","",""];
 var printAry = ["","","","","","","","","","","","","","","","",""];
 
-//---------------------------------------------------
-// these lines create variables using moment.js
-// and then console.log out those variables to show
-// the format that moment gives for each. there is 
-// more info in the docs on different options and 
-// formatting.
-//---------------------------------------------------
+
 var dayDate = moment().format("dddd, MMMM Do YYYY");
 var currentHour = moment().format("HH");
-// saveAry.push(dayDate);
 
-console.log(currentHour);
-console.log(dayDate);
-
-//---------------------------------------------------
 if (localStorage.getItem("Planner Save") === null){
     localStorage.setItem("Planner Save", saveAry);
 }
 
 $(".date").text(dayDate);
 
-function saveClick (event) { //function that is run when save button is clicked
+function saveClick (event) { 
     event.preventDefault();
     var time = $(this).attr("data-time")
     var test2 = $(this).siblings();
@@ -32,12 +21,9 @@ function saveClick (event) { //function that is run when save button is clicked
     var storageAry = saveAry.toString();
     localStorage.setItem("Planner Save", storageAry);
     pullStorage();
-    
-
-    //need to rename some variables and convert array into string and store in local storage.
 }
 
-function pullStorage () { //function to get info from local storage and populate input fields to contain info from storage
+function pullStorage () {
     printAry = localStorage.getItem("Planner Save");
     printAry = printAry.split(",");
     if (localStorage.getItem("date") === dayDate){
@@ -59,10 +45,5 @@ function pullStorage () { //function to get info from local storage and populate
 }
 pullStorage();
 saveAry = printAry;
-// if (dayDate !== printAry[0]){
-//     saveAry = printAry;
-// } else {
-//     saveAry = [];
-// }
 
 $(".planner").on("click", "button", saveClick)
